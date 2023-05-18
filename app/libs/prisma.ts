2a0dac4acb264f5c0915064ebaddbd29ca47feda
iamMAHAM <abdulkabore@gmail.com>
2023-05-18 06:59:46 +0000

    update prisma

--- app/libs/prisma.ts
+++ app/libs/prisma.ts
@@ -6,9 +6,9 @@ declare global {
 }
 
 // eslint-disable-next-line no-undef
-const client = globalThis.prisma ?? new PrismaClient();
+const prisma = globalThis.prisma ?? new PrismaClient();
 
 // eslint-disable-next-line no-undef
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = client;
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
 
-export default client;
+export default prisma;
